d4deefb5f3ffb651988c69889bf3480fd974ddaa
77web <info@77-web.com>
2021-12-15 22:46:44 +0900

    added "add a Task" action

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Form\TaskType;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     public function __construct(
-        private TaskRepository $taskRepository
+        private TaskRepository $taskRepository,
+        private FormFactoryInterface $formFactory,
+        private EntityManagerInterface $em,
     ) {
     }
 
@@ -19,6 +26,23 @@ class DefaultController extends AbstractController
     {
         return $this->render('default/index.html.twig', [
             'tasks' => $this->taskRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new')]
+    public function new(Request $request): Response
+    {
+        $form = $this->formFactory->create(TaskType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($form->getData());
+            $this->em->flush();
+
+            return $this->redirectToRoute('default');
+        }
+
+        return $this->renderForm('default/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
